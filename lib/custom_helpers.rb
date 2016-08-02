7c33f3f7c52013584c6e5d1419f83bde324c2885
ed wassermann <vis0vis@gmail.com>
2016-08-02 16:40:53 +0200

    Extract helpers from config.rb into lib directory
    Add new custom_helpers file

---
+++ lib/custom_helpers.rb
@@ -0,0 +1,35 @@
+module CustomHelpers
+
+  def print_title
+    '- ' + current_article.title unless current_article.nil?
+  end
+
+  def dasherize(name)
+    name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def interviewee
+    current_page.data.interviewee
+  end
+
+  def date
+    current_article.date.strftime('%b %e | %Y')
+  end
+
+  def title
+    current_article.title
+  end
+
+  def topic_list
+    current_article.data.topic_list
+  end
+
+  def soundcloud_id
+    current_page.data.soundcloud_id
+  end
+
+  def big_soundcloud_player
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/"+ soundcloud_id.to_s + "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
+  end
+
+end
